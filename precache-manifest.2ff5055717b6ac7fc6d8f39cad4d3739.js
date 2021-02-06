13343ee44e88a61e5c43ec086374cfd85eda8dbe
Yuri <dog-pit10@yandex.ru>
2021-02-06 16:38:57 +0300

    Updates

---
+++ precache-manifest.2ff5055717b6ac7fc6d8f39cad4d3739.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9dee5a3c591822ebb7f49d12e6b30d5",
+    "url": "/tasklist/index.html"
+  },
+  {
+    "revision": "271a9c93915a7c03c231",
+    "url": "/tasklist/static/css/main.4deb9e60.chunk.css"
+  },
+  {
+    "revision": "37056366b208ca4f8e57",
+    "url": "/tasklist/static/js/2.4f8d20ec.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tasklist/static/js/2.4f8d20ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "271a9c93915a7c03c231",
+    "url": "/tasklist/static/js/main.e7f6f80b.chunk.js"
+  },
+  {
+    "revision": "57ca3cb77f47b68f926b",
+    "url": "/tasklist/static/js/runtime-main.12159b6b.js"
+  }
+]);
